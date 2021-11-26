05f07feb3c366d56e2befa247a643f3c4cbf7981
Lincxx <bossco28@gmail.com>
2021-11-26 11:23:40 -0500

    Operators basics

---
+++ Section02 Ruby Programing/03_comparison.rb
@@ -0,0 +1,18 @@
+# -- operators
+# ==
+# != 
+# <
+# >
+# >=
+# <=
+
+puts 1 == 1
+puts 1 < 1
+puts 1 > 1
+puts 1 != 1
+puts 1 <= 1
+puts 1 >= 1
+
+
+# -- .eql?
+puts 10.eql?(10.0) # this compare types
