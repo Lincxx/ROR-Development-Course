bb2b47d793c0c4ec20233f44fc70f48bb62b657b
Lincxx <bossco28@gmail.com>
2021-11-26 11:57:10 -0500

    Methods

---
+++ Section02 Ruby Programing/04_methods.rb
@@ -0,0 +1,13 @@
+# -- Method
+
+def multiply(num_1, num_2)
+    num_1 * num_2
+end
+
+puts multiply(3,4)
+
+def greet(name, msg)
+    puts "#{msg} #{name.capitalize}"
+end
+
+greet("jeremy", "hello there,")
